ac93a4621df597d93d531ac3762988aa95fdae0c
nadunchanna98 <gancp1218@gmail.com>
2023-02-21 22:23:08 +0530

    user operation done without login

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -18,7 +18,9 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping("/user")
-    User newUser(@RequestBody User newUser){
+    User newUser(@RequestBody User newUser) throws Exception {
+        String encryptedPassword = AESEncryption.encrypt(newUser.getPassword());
+        newUser.setPassword(encryptedPassword);
         return userRepository.save(newUser);
     }
 
@@ -27,15 +29,78 @@ public class UserController {
         return userRepository.findAll();
     }
 
-   @GetMapping("/users/user/{id}")
-    User getUserById(@PathVariable Long id){
-        return userRepository.findById(id)
-                .orElseThrow(()->new UserNotFoundException(id));
-   }
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) throws Exception {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+        String decryptedPassword = AESEncryption.decrypt(user.getPassword());
+        user.setPassword(decryptedPassword);
+        return user;
+    }
 
-   @GetMapping("/users/user/email/{email}")
+   @GetMapping("/user/email/{email}")
    Object checkEmailExists(@PathVariable String email){
         return userRepository.findByEmail(email);
    }
 
-}
+    @PutMapping("/user/change/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    if (newUser.getUsername() != null) {
+                        user.setUsername(newUser.getUsername());
+                    }
+                    if (newUser.getEmail() != null) {
+                        user.setEmail(newUser.getEmail());
+                    }
+                    if (newUser.getAddress() != null) {
+                        user.setAddress(newUser.getAddress());
+                    }
+                    if (newUser.getPhone() != null) {
+                        user.setPhone(newUser.getPhone());
+                    }
+                    if (newUser.getName() != null) {
+                        user.setName(newUser.getName());
+                    }
+                    if (newUser.getTitle() != null) {
+                        user.setTitle(newUser.getTitle());
+                    }
+                    if (newUser.getPassword() != null) {
+                        String hashedPassword = PasswordEncoder.encode(newUser.getPassword());
+                        user.setPassword(hashedPassword);
+                    }
+                    return userRepository.save(user);
+                })
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+
+        userRepository.deleteById(id);
+        return "User with id "+id+" has been deleted success. ";
+    }
+
+    @PostMapping("/user/login")
+    public String login(@RequestBody User request) throws Exception {
+        String username = request.getUsername();
+        String password = request.getPassword();
+        // retrieve the user from the database by username
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return "Invalid username or password";
+        }
+        String encryptedPassword = user.getPassword();
+        String decryptedPassword = AESEncryption.decrypt(encryptedPassword);
+        if (password.equals(decryptedPassword)) {
+            return "Login successful";
+        } else {
+            return "Invalid username or password";
+        }
+    }
+    
+
+        }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User,Long>{
 
 
     Optional<Object> findByEmail(String email);
+
+    User findByUsername(String username);
 }
